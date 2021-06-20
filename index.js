881e2642fd25aa2dad1624affe01fae4c07c170a
Junaid Rahim <junaidrahim8d@gmail.com>
2021-06-20 23:25:16 +0530

    html update

--- index.js
+++ index.js
@@ -2,40 +2,44 @@ const authorize_notion = async () => {
     const url = new URL(window.location.href);
     const searchParams = new URLSearchParams(url.searchParams);
     const temp_auth_code = searchParams.get("code");
-    console.log("temp_auth_code: ", temp_auth_code)
 
     const req_body = {
         code: temp_auth_code,
     };
 
     if (temp_auth_code != undefined) {
-        const response = await fetch(
-            "https://arxivtonotion.herokuapp.com/auth",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(req_body),
-            }
-        );
-        $('#auth-successful').show();
+        try {
+            const response = await fetch(
+                "https://arxivtonotion.herokuapp.com/auth",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(req_body),
+                }
+            );
 
-        const notion_auth_data = await response.json();
-        console.log(notion_auth_data)
-        send_to_extension(notion_auth_data);
+            const notion_auth_data = await response.json();
+            send_to_extension(notion_auth_data);
+            $("#auth-successful").show();
+        } catch (err) {
+            console.log(err);
+            $("#auth-unsuccessful").show();
+        }
     } else {
-        $('#auth-unsuccessful').show();
-        console.log("error in url parameters")
+        $("#auth-unsuccessful").show();
+        console.log("error in url parameters");
     }
 };
 
 const send_to_extension = (data) => {
     const extension_id = "mphoppdgbgknlpfpmkakoegfkidfblel";
-
-    chrome.runtime.sendMessage(extension_id, data, (response) =>
-        console.log(response)
-    );
+    chrome.runtime.sendMessage(extension_id, data, (response) => {
+        if (response != "auth data set successfully") {
+			throw Error("Invalid response from chrome-extension: ", response)
+        }
+    });
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
